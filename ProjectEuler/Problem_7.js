d4541e53aae51c68b73ad3ae68782fd6e7a8ba49
uoo38 <plus.wisteria38@gmail.com>
2017-05-02 08:54:58 +0900

    modified Problem_7.js

--- ProjectEuler/Problem_7.js
+++ ProjectEuler/Problem_7.js
@@ -1,13 +1,13 @@
 const N = 10001;
-const M = 100000000000;
+const M = 1000000;
 
 let num = [];
 let prime = [];
 
-for (let i = 2; i <= Math.sqrt(M); i++) {
+for (let i = 2; i <= M; i++) {
   if (!num[i]) {
     prime.push(i);
-    for (let j = 2; i*j <= Math.sqrt(M); j++) {
+    for (let j = 2; i*j <= M; j++) {
       num[i*j] = true;
     }
   }

60a7ea62ffac3343a7b8bb863aee3d332aa547b4
uoo38 <plus.wisteria38@gmail.com>
2017-05-04 22:18:40 +0900

    solved Problem 12

---
+++ ProjectEuler/Problem_12.js
@@ -0,0 +1,25 @@
+const N = 500;
+const M = 100000;
+
+let tri = [];
+
+tri.push(0);
+for (let i = 1; i <= M; i++) {
+  tri.push(tri[i-1] + i);
+  if (divisor_num(tri[i]) > 500) {
+    console.log(tri[i]);
+    break;
+  }
+}
+
+function divisor_num(x) {
+  let ret = 0;
+  for (let i = 1; i*i <= x; i++) {
+    if (x % i === 0) {
+      ret++;
+      if (i !== x/i)
+        ret++;
+    }
+  }
+  return ret;
+}

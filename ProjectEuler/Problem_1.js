5a7229c7e1513a487c946309ba50ddbf95c17571
uoo38 <plus.wisteria38@gmail.com>
2017-05-06 21:07:00 +0900

    modified Problem_1.js

--- ProjectEuler/Problem_1.js
+++ ProjectEuler/Problem_1.js
@@ -2,8 +2,8 @@ let N = 1000;
 let ans = 0;
 
 for (let i = 0; i < N; i++) {
-    if (i % 3 === 0 || i % 5 === 0)
-        ans += i;
+  if (i % 3 === 0 || i % 5 === 0)
+    ans += i;
 }
 
 console.log(ans);

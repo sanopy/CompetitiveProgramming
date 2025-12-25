799a8f15bbbdc54d3a7325e067dbe52d7460c671
Sanosuke Kato <8940110+sanopy@users.noreply.github.com>
2025-12-26 01:23:16 +0900

    solved 1. Two Sum (brute force)

---
+++ LeetCode/0001/two-sum/src/main.rs
@@ -0,0 +1,22 @@
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        for i in 0..nums.len() {
+            for j in (i + 1)..nums.len() {
+                if nums[i] + nums[j] == target {
+                    return vec![i as i32, j as i32];
+                }
+            }
+        }
+        return vec![];
+    }
+}
+
+// -----------------------------------------------------------------------------------------------------------------
+
+struct Solution;
+
+fn main() {
+    println!("{:?}", Solution::two_sum( vec![2, 7, 11, 15], 9));
+    println!("{:?}", Solution::two_sum( vec![3, 2, 4], 6));
+    println!("{:?}", Solution::two_sum( vec![3, 3], 6));
+}
